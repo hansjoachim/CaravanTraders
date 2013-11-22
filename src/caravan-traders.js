72e6f4a081bfdbe84d0a58e8c5ddcc7841a55d94
Hans Joachim Desserud <github@desserud.org>
2013-11-23 00:04:20 +0100

    Reorganize import of angular, and wrap it in a module

--- src/caravan-traders.js
+++ src/caravan-traders.js
@@ -1,10 +1,18 @@
-define(function(require) {
-  var angular = require(["../dependencies/angular"]);
-  var city = require("city");
+require.config({
+  paths: {
+    angular: '../dependencies/angular'
+  },
+  shim: {
+    'angular': {'exports': 'angular'}
+  }
+});
+
+require(["angular", "city"], function(angular, city) {
+//  angular.bootstrap(document);
+
   /*  function CityController($scope) {
       $scope.city = {wares: ["foo", "bar"]}
     }*/
-
   var some_place = city.create(["something", "something else", "yet another thing"]);
   console.log(some_place.wares);
 });
