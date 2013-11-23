73ef17337d3d0e6b2e5ef7993db6b8db36e3faee
Hans Joachim Desserud <github@desserud.org>
2013-11-23 21:09:05 +0100

    Added a button to purchase wares. So fare only purchase one at a time, and prints the bought item in the console

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ define(
       $scope.city = city.create("Somewhere", [ware.create("apple", 5),
                                               ware.create("banana", 1),
                                               ware.create("orange", 2)]);  
+      $scope.purchase = function(ware) {
+        var purchased = $scope.city.sell(ware, 1);
+        console.log("Player bought " + purchased.amount + " " + purchased.name);
+    };
     }]);
 
     return angular.module("app");
