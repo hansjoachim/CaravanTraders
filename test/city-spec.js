294e3f4b05f2000db60e40723b65505a84462cb6
Hans Joachim Desserud <github@desserud.org>
2013-12-01 14:59:31 +0100

    Light linting

--- test/city-spec.js
+++ test/city-spec.js
@@ -1,3 +1,4 @@
+/*jslint indent: 2 */
 var city = require("../src/city");
 var ware = require("../src/ware");
 
@@ -5,28 +6,29 @@ var apples = ware.create("apple", 2);
 var banana = ware.create("banana");
 var orange = ware.create("orange");
 
-describe("a city", function() {
-  it("has a name", function() {
+describe("a city", function () {
+  "use strict";
+  it("has a name", function () {
     var some_place = city.create("hello");
     expect(some_place.name).toBe("hello");
   });
-  it("has zero wares unless otherwise specified", function() {
+  it("has zero wares unless otherwise specified", function () {
     var some_place = city.create("name");
     expect(some_place.wares.length).toBe(0);
   });
-  it("can contain wares", function() {
+  it("can contain wares", function () {
     var some_place = city.create("some_place", [banana]);
     expect(some_place.wares.length).toBe(1);
     expect(some_place.wares[0].name).toBe("banana");
   });
-  it("can get an initial list with multiple wares", function() {
+  it("can get an initial list with multiple wares", function () {
     var pear = ware.create("pear");
     var city_with_multiple_wares = city.create("multiplexia", [orange, pear]);
     expect(city_with_multiple_wares.wares.length).toEqual(2);
     expect(city_with_multiple_wares.wares).toContain(orange);
     expect(city_with_multiple_wares.wares).toContain(pear);
   });
-  it("can remove some of a ware", function() {
+  it("can remove some of a ware", function () {
     var bananas = ware.create("banana", 3);
     var some_place = city.create("some place", [bananas]);
     var sold = some_place.removeWare("banana", 2);
@@ -35,14 +37,14 @@ describe("a city", function() {
     expect(some_place.wares.length).toBe(1);
     expect(some_place.getAmount("banana")).toBe(1);
   });
-  it("will stop listing a ware if all of it is removed", function() {
+  it("will stop listing a ware if all of it is removed", function () {
     var some_place = city.create("some place", [banana]);
     var sold = some_place.removeWare("banana", 1);
     expect(sold.name).toBe("banana");
     expect(sold.amount).toBe(1);
     expect(some_place.wares.length).toBe(0);
   });
-  it("can remove all of one ware without impacting the others", function() {
+  it("can remove all of one ware without impacting the others", function () {
     var place = city.create("name", [banana, apples]);
     var all_apples = place.removeWare("apple", 2);
     expect(all_apples.name).toBe("apple");
@@ -51,7 +53,7 @@ describe("a city", function() {
     expect(place.getWareNames()).not.toContain("apple");
     expect(place.getWareNames()).toContain("banana");
   });
-  it("will remove all of a ware if an amount is not specified", function() {
+  it("will remove all of a ware if an amount is not specified", function () {
     var place = city.create("name", [orange, apples]);
     var all_apples = place.removeWare("apple");
     expect(all_apples.name).toBe("apple");
@@ -62,23 +64,23 @@ describe("a city", function() {
    //TODO: long-term: combine buying/selling in a purchase-object
   //TODO: should throw an exception if I attempt to remove a ware I don't have
   //TODO: ditto for attempting to remove a too large amount
-  it("has a list of the wares in stock", function() {
+  it("has a list of the wares in stock", function () {
     var wares = [banana, orange];
     var town = city.create("name", wares);
     expect(town.getWareNames().length).toBe(2);
     expect(town.getWareNames()).toContain("banana");
     expect(town.getWareNames()).toContain("orange");
   });
-  it("can get the amount of a ware", function() {
+  it("can get the amount of a ware", function () {
     var some_place = city.create("some place", [banana, apples]);
     expect(some_place.getAmount("banana")).toBe(1);
     expect(some_place.getAmount("apple")).toBe(2);
   });
-  it("can say it has zero of a ware it doesn't have in stock", function() {
+  it("can say it has zero of a ware it doesn't have in stock", function () {
     var some_place = city.create("some place", []);
     expect(some_place.getAmount("banana")).toBe(0);
   });
-  it("can store different amounts of wares", function() {
+  it("can store different amounts of wares", function () {
     var wares = [banana, apples];
     var town = city.create("name", wares);
     expect(town.wares[0].name).toBe("banana");
--- test/player-spec.js
+++ test/player-spec.js
@@ -1,30 +1,32 @@
+/*jslint indent: 2 */
 var city = require("../src/city");
 var player = require("../src/player");
 var ware = require("../src/ware");
 
 var p1 = player.create(city.create("starting point"));
 
-describe("player", function() {
-  it("starts with an empty list of wares", function() {
+describe("player", function () {
+  "use strict";
+  it("starts with an empty list of wares", function () {
     expect(p1.wares.length).toBe(0);
   });
-  it("starts with 10 gold", function() {
+  it("starts with 10 gold", function () {
     expect(p1.gold).toBe(10);
   });
-  it("knows the current location", function() {
+  it("knows the current location", function () {
     expect(p1.location.name).toBe("starting point");
   });
-  it("can travel", function() {
+  it("can travel", function () {
     p1.travelTo(city.create("somewhere else"));
     expect(p1.location.name).toBe("somewhere else");
   });
-  it("can gain a ware", function() {
+  it("can gain a ware", function () {
     p1.addWare(ware.create("banana"));
     expect(p1.wares.length).toBe(1);
     expect(p1.wares[0].name).toBe("banana");
     expect(p1.wares[0].amount).toBe(1);
   });
-  it("can gain multiple wares", function() {
+  it("can gain multiple wares", function () {
     var trader = player.create(city.create("Someplace"));
     trader.addWare(ware.create("apple"));
     trader.addWare(ware.create("orange"));
@@ -34,7 +36,8 @@ describe("player", function() {
     expect(trader.wares[1].name).toBe("orange");
     expect(trader.wares[1].amount).toBe(1);
   });
-  it("will stack wares of the same type when receiving more", function() {
+  it("will stack wares of the same type when receiving more", function () {
+  "use strict";
     var a_banana = ware.create("banana");
     var another_banana = ware.create("banana");
     var trader = player.create(city.create("Someplace"));
