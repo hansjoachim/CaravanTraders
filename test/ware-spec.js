1049a54e6b6d598460545db0ec949827991b8b22
Hans Joachim Desserud <github@desserud.org>
2013-12-15 19:19:16 +0100

    Started refactoring to get the test suite running with karma test runner. So far, the ware suite can run. (Might have broken everything else in the process though)

--- test/ware-spec.js
+++ test/ware-spec.js
@@ -1,19 +1,19 @@
 /*jslint indent: 2 */
-var ware = require("../src/ware");
-
-describe("a ware", function () {
-  "use strict";
-  it("has a name", function () {
-    var banana = ware.create("banana");
-    expect(banana.name).toBe("banana");
-  });
-  it("has an amount", function () {
-    var apple = ware.create("apple", 3);
-    expect(apple.name).toBe("apple");
-    expect(apple.amount).toBe(3);
-  });
-  it("has default amount of 1", function () {
-    var orange = ware.create("orange");
-    expect(orange.amount).toBe(1);
+define(['ware'], function (ware) {
+  describe("a ware", function () {
+    "use strict";
+    it("has a name", function () {
+      var banana = ware.create("banana");
+      expect(banana.name).toBe("banana");
+    });
+    it("has an amount", function () {
+      var apple = ware.create("apple", 3);
+      expect(apple.name).toBe("apple");
+      expect(apple.amount).toBe(3);
+    });
+    it("has default amount of 1", function () {
+      var orange = ware.create("orange");
+      expect(orange.amount).toBe(1);
+    });
   });
 });
