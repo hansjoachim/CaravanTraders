7059b4b62faa1ca5152c83cff5e3de43caacf982
Hans Joachim Desserud <github@desserud.org>
2013-12-19 18:17:25 +0100

    Get rid of boilerplate and code for testing the waters

--- test/app-spec.js
+++ test/app-spec.js
@@ -1,32 +1,25 @@
-define(['angular', 'angularmocks'], function (angular, angularmocks) {
+define(['angularmocks'], function (mock) {
   "use strict";
   describe('app', function () {
-    beforeEach(angularmocks.module('app'));
+    beforeEach(mock.module('app'));
 
-    it('has angular available', function () {
-      expect(angular).not.toBe(undefined);
-    });
-    it('has angularmocks available too', function() {
-      expect(angularmocks).not.toBe(undefined);
-    });
+    var scope, ctrl;
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('CityController', {$scope:scope});
+    }));
+
     it('creates two cities by default', inject(function($controller) {
-      var scope = {};
-      var ctrl = $controller("CityController", {$scope:scope});
       expect(scope.cities.length).toBe(2);
     }));
     it('creates a player by default', inject(function($controller) {
-      var scope = {};
-      var ctrl = $controller("CityController", {$scope:scope});
       expect(scope.player.gold).toBe(10);
       expect(scope.player.wares.length).toBe(0);
     }));
     it('allows a player to travel to another place', inject(function($controller) {
-      var scope = {};
-      var ctrl = $controller("CityController", {$scope:scope});
       var starting_place = scope.player.location;
       scope.travelTo(scope.cities[1]);
       expect(scope.player.location).not.toBe(starting_place);
     }));
-
   });
 });
