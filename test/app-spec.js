6a57c000ce02299eda3c230c96b074905e194957
Hans Joachim Desserud <github@desserud.org>
2013-12-19 18:05:32 +0100

    Verify player traveling is working. Refactor creation of cities

--- test/app-spec.js
+++ test/app-spec.js
@@ -9,10 +9,24 @@ define(['angular', 'angularmocks'], function (angular, angularmocks) {
     it('has angularmocks available too', function() {
       expect(angularmocks).not.toBe(undefined);
     });
-    it('has two cities by default', inject(function($controller) {
+    it('creates two cities by default', inject(function($controller) {
       var scope = {};
       var ctrl = $controller("CityController", {$scope:scope});
       expect(scope.cities.length).toBe(2);
     }));
+    it('creates a player by default', inject(function($controller) {
+      var scope = {};
+      var ctrl = $controller("CityController", {$scope:scope});
+      expect(scope.player.gold).toBe(10);
+      expect(scope.player.wares.length).toBe(0);
+    }));
+    it('allows a player to travel to another place', inject(function($controller) {
+      var scope = {};
+      var ctrl = $controller("CityController", {$scope:scope});
+      var starting_place = scope.player.location;
+      scope.travelTo(scope.cities[1]);
+      expect(scope.player.location).not.toBe(starting_place);
+    }));
+
   });
 });
