5da95f08dae01540e6e6225ef6dfde4e5938a63f
Hans Joachim Desserud <github@desserud.org>
2013-12-19 17:28:22 +0100

    Get a real testcase running, verifying one of the controllers

--- test/app-spec.js
+++ test/app-spec.js
@@ -1,11 +1,18 @@
 define(['angular', 'angularmocks'], function (angular, angularmocks) {
   "use strict";
   describe('app', function () {
+    beforeEach(angularmocks.module('app'));
+
     it('has angular available', function () {
       expect(angular).not.toBe(undefined);
     });
     it('has angularmocks available too', function() {
       expect(angularmocks).not.toBe(undefined);
     });
+    it('has two cities by default', inject(function($controller) {
+      var scope = {};
+      var ctrl = $controller("CityController", {$scope:scope});
+      expect(scope.cities.length).toBe(2);
+    }));
   });
 });
