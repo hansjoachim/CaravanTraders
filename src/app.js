f025be8a94e88346f2730b001d7e3c15ca6d7b7f
Hans Joachim Desserud <github@desserud.org>
2013-12-25 12:16:07 +0100

    Nitpicky style fixes

--- src/app.js
+++ src/app.js
@@ -10,30 +10,31 @@ define(
     var createCities = function () {
       var somewhere = city.create("Somewhere", [ware.create("apple", 5),
                                                 ware.create("banana", 1),
-                                                ware.create("orange", 2)]); 
+                                                ware.create("orange", 2)]);
       var secondCity = city.create("Otherplace", [ware.create("apple", 2),
                                                  ware.create("pear", 4)]);
       return [somewhere, secondCity];
     };
 
-    app.controller("CityController", ["$scope", function($scope) {
+    app.controller("CityController", ["$scope", function ($scope) {
       $scope.cities = createCities();
       $scope.player = player.create($scope.cities[0]);
 
-      $scope.purchase = function(ware) {
+      $scope.purchase = function (ware) {
         var purchased = $scope.player.location.removeWare(ware, 1);
         $scope.player.addWare(purchased);
       };
 
-      $scope.sell = function(ware) {
+      $scope.sell = function (ware) {
         var sold = $scope.player.removeWare(ware, 1);
         $scope.player.location.addWare(sold);
       };
 
-      $scope.travelTo = function(destination) {
+      $scope.travelTo = function (destination) {
         $scope.player.travelTo(destination);
       };
     }]);
 
     return app;
-});
+  }
+);
